321f6e88c8c8fc91173b483fd7ca39c69b75cffd
throskam <throskam@gmail.com>
2019-05-04 18:35:31 +0200

    feat: improve response generator configuration
    Use the most appropriate status and media type automatically.

--- src/lib/middlewares/responseGenerator.js
+++ src/lib/middlewares/responseGenerator.js
@@ -4,12 +4,36 @@ module.exports = (option = {}) => {
       return next()
     }
 
+    const responses = ctx.state.oas.operation.responses
+    const statusKeys = Object.keys(responses)
+    const numericals = statusKeys.filter(Number)
+    const successes = numericals.filter(n => n >= 200 && n < 300)
+    const redirections = numericals.filter(n => n >= 300 && n < 400)
+
+    // Choose the most approriate status key (2XX, 3XX, default or first)
+    const statusKey = successes.length ? Math.min(successes)
+      : responses['2XX'] ? '2XX'
+        : redirections.length ? Math.min(redirections)
+          : responses['3XX'] ? '3XX'
+            : responses['default'] ? 'default'
+              : statusKeys[0]
+
+    // Choose the most appropriate media type (default or first)
+    const mediaType = responses[statusKey].content[option.defaultResponseContentType]
+      ? option.defaultResponseContentType
+      : Object.keys(responses[statusKey].content)[0]
+
+    // Convert the status key to a valid status code.
+    const status = !isNaN(Number(statusKey)) ? Number(statusKey)
+      : statusKey.includes('XX') ? statusKey.slice(0, 1) * 100
+        : 200
+
     ctx.state.oas.response = {
-      status: 200,
-      mediaType: option.defaultResponseContentType,
+      status,
+      mediaType,
       ...ctx.state.oas.impl.responseGenerator({
-        status: 200,
-        mediaType: option.defaultResponseContentType
+        status,
+        mediaType
       })
     }
 
